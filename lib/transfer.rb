0f1ed26a58a36b92911528954c5f0345ed74841b
Joseph Perez <hello@josephdanielperez.com>
2020-07-30 22:37:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       self.status = "complete"
     elsif self.status == "complete"
       "too bad too sad"
-    else sender.balance < amount
+    else sender.balance < @amount
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
